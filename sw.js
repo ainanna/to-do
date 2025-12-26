da92d2471129dc42dc074d630834db8098f939a0
ainanna <hanniandrea10@gmail.com>
2025-12-27 00:28:12 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
-const CACHE="aion2-tw-final";
-const FILES=["./","./index.html","./manifest.json"];
-
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("aion2-cache").then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.json"
+      ])
+    )
+  );
 });
 
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
