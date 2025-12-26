3da31b8a8e75af40d05fb474ad3c0cfa05fa806e
ainanna <hanniandrea10@gmail.com>
2025-12-26 13:06:23 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE="aion2-tw-final";
+const FILES=["./","./index.html","./manifest.json"];
+
+self.addEventListener("install",e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+
+self.addEventListener("fetch",e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
